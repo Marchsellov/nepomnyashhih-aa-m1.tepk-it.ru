bfc189743a5fd8f2191bb89267733d2d957a4632
gitUser <gitUser@gmail.com>
2025-06-04 11:38:58 +0700

    Some changes(0)

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=nepomnyashhih-aa_m1',
+    'username' => 'nepomnyashhih-aa',
+    'password' => 'PassWord349!',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
